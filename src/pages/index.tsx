e4987dab390953a5796eae822b821a1093be3d66
Rouf Majidin <roufmajidin777@gmail.com>
2024-11-08 05:47:54 +0700

    dynamic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,8 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import dynamic from 'next/dynamic';
-import { useEffect } from 'react';
-
 import { Inter } from 'next/font/google'
+import dynamic from 'next/dynamic';
+
 import styles from '@/styles/Home.module.css'
 const Navbar = dynamic(() => import('../components/Navbar'), {
   ssr: false, // Disable server-side rendering
