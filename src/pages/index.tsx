e4aaf7b060214687856038c5ca96f6badd116fac
Rouf Majidin <roufmajidin777@gmail.com>
2024-11-08 05:41:34 +0700

    peww

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,13 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import dynamic from 'next/dynamic';
+import { useEffect } from 'react';
+
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import Navbar from '../components/navbar'
+const Navbar = dynamic(() => import('../components/Navbar'), {
+  ssr: false, // Disable server-side rendering
+});
 import HomeP from '../components/home'
 const inter = Inter({ subsets: ['latin'] })
 
