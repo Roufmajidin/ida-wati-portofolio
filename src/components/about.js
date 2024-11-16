7896ad0bcb73a89c6898d99702ac5d3d1c5d9366
Mataharibulan123# <idaw9765@gmail.com>
2024-11-16 23:13:00 +0700

    update jadi dinamis

--- src/components/about.js
+++ src/components/about.js
@@ -1,83 +1,140 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-const About = ()=>{
-    return(
-        <>
-        <header id="header" className="header">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-lg-8 mx-auto">
-                            <div className="text-container text-center">
-                                <h1 className="h1-large">IDA WATI</h1>
-                                <p style={{ color: "white" }}>
-                                Sebagai lulusan S1 Sistem Informasi dari Universitas Catur Insan Cendekia dengan gelar S.kom. Saya memiliki pengetahuan dan minat tinggi untuk berkarir di bidang teknologi, administrasi, data entry, business analyst, UI/UX Designer, dan mampu mengoperasikan microsoft word, microsoft powerpoint dan microsoft excel. Saya memiliki sifat pantang menyerah, ingin terus belajar hal-hal baru dan mudah beradaptasi dengan lingkungan dan saya siap untuk memberikan kontribusi positif dalam tim dengan fokus pada tujuan perusahaan.
-                                </p>
-                                
-                                <a className="btn-solid-lg page-scroll" href="#about" style={{ borderRadius: "10px" }} >About me</a>
-                                <a className="btn-outline-lg page-scroll" href="https://medium.com/@idaw9765"><i className="fas fa-user" />Medium</a>
-                            </div> {/* end of text-container */}
-                        </div> {/* end of col */}
-                    </div> {/* end of row */}
-                </div> {/* end of container */}
-            </header>
+const About = () => {
+  // Data array
+  const education = [
+    {
+      title: "Universitas Catur Insan Cendekia",
+      subtitle: "Sistem Informasi",
+      time: "2020 - 2024 (IPK 3.91)",
+    },
+  ];
 
-            <div id="about" className="basic-1 bg-gray">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-lg-4">
-                            <div className="text-container first">
-                                <h2>Pendidikan</h2>
-                                <h6>Universitas Catur Insan Cendekia</h6>
-                                <h6>Sistem Informasi</h6>
-                                <div className="time">2020 - 2024 (IPK 3.91)</div>
-                                <br></br>
+  const certifications = [
+    {
+      time: "Oktober 2024",
+      title: "Pemakalah Seminar Nasional dan Teknologi (SISFOTEK)",
+    },
+    {
+      time: "September 2023 - Januari 2024",
+      title: "MAGANG MANDIRI PTP TERMINAL NONPETIKEMAS BRANCH CIREBON",
+      description: "Pendukung Operasional dan Teknik HSSE",
+    },
+  ];
 
-                                <h2>Sertifikasi</h2>
-                                <div className="time">Oktober 2024</div>
-                                <h6>Pemakalah Seminar Nasional dan Teknologi (SISFOTEK)</h6>
+  const experiences = [
+    {
+      time: "Februari - Juni 2023",
+      title: "Studi Independen",
+      description: "STUDI INDEPENDENT KAMPUS MERDEKA DI PT. MARKA KREASI PERSADA (ALTERRA ACADEMY) Malang, Indonesia",
+    },
+    {
+      time: "April – Juni 2022",
+      title: "GOOGLE IDN TRAINING – Jakarta, Indonesia",
+      description: "IT Support Google",
+    },
+    {
+      time: "Februari 2023",
+      title: "Dicoding Academy Indonesia",
+      description: "Pelatihan Online (Memulai Pemrograman Dengan Java)",
+    },
+  ];
 
-                                <br></br>
+  const achievements = [
+    {
+      time: "2022",
+      title: "Himasi Festival 2022",
+      description: "Juara 1 Tingkat Ciayumajakuning kategori lomba tata tulis karya ilmiah dalam acara Himasi Festival 2022",
+    },
+    {
+      time: "2022",
+      title: "Himasi Festival 2022",
+      description: "Juara 2 Tingkat Ciayumajakuning kategori lomba Business Plan dalam acara Himasi Festival 2022",
+    },
+  ];
 
-                                <div className="time">September 2023 - Januari 2024</div>
-                                <h6>MAGANG MANDIRI PTP TERMINAL NONPETIKEMAS BRANCH CIREBON </h6>
-                                <p>Pendukung Operasional dan Teknik HSSE</p>
-                                
-                                
-                            </div> {/* end of text-container */}
-                        </div> {/* end of col */}
-                        <div className="col-lg-4">
-                            <div className="text-container second">
-                                
-
-                                <div className="time">Februari - Juni 2023</div>
-                                <h6>Studi Independen</h6>
-                                <p>STUDI INDEPENDENT KAMPUS MERDEKA DI PT. MARKA KREASI PERSADA (ALTERRA ACADEMY) Malang, Indonesia</p>
-                                
-                                <div className="time">April – Juni 2022</div>
-                                <h6>GOOGLE IDN TRAINING – Jakarta, Indonesia</h6>
-                                <p>IT Support Google </p>
-
-                                <div className="time">Februari 2023</div>
-                                <h6>Dicoding Academy Indonesia</h6>
-                                <p>Pelatihan Online (Memulai Pemrograman Dengan Java)</p>
-                             
-                            </div>
-                            
-                        </div> {/* end of col */}
-                        <div className="col-lg-4">
-                            <div className="text-container third">
-                                <div className="time">2022</div>
-                                <h6>Himasi Festival 2022</h6>
-                                <p>Juara 1 Tingkat Ciayumajakuning kategori lomba <b><i>tata tulis karya ilmiah</i></b> dalam acara Himasi Festival 2022 </p>
-                                <div className="time">2022</div>
-                                <h6>Himasi Festival 2022</h6>
-                                <p>Juara 2 Tingkat Ciayumajakuning kategori lomba <b><i>Business Plan</i> </b> dalam acara Himasi Festival 2022</p>
-                            </div> {/* end of text-container */}
-                        </div> {/* end of col */}
-                    </div> {/* end of row */}
-                </div> {/* end of container */}
+  return (
+    <>
+      {/* Header Section */}
+      <header id="header" className="header">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-8 mx-auto">
+              <div className="text-container text-center">
+                <h1 className="h1-large">IDA WATI</h1>
+                <p style={{ color: "white" }}>
+                  Sebagai lulusan S1 Sistem Informasi dari Universitas Catur Insan Cendekia dengan gelar S.kom. Saya memiliki pengetahuan dan minat tinggi untuk berkarir di bidang teknologi, administrasi, data entry, business analyst, UI/UX Designer, dan mampu mengoperasikan microsoft word, microsoft powerpoint dan microsoft excel. Saya memiliki sifat pantang menyerah, ingin terus belajar hal-hal baru dan mudah beradaptasi dengan lingkungan dan saya siap untuk memberikan kontribusi positif dalam tim dengan fokus pada tujuan perusahaan....
+                </p>
+                <a className="btn-solid-lg page-scroll" href="#about" style={{ borderRadius: "10px" }}>
+                  About me
+                </a>
+                <a className="btn-outline-lg page-scroll" href="https://medium.com/@idaw9765">
+                  <i className="fas fa-user" /> Medium
+                </a>
+              </div>
             </div>
-        </>
-    )
-}
+          </div>
+        </div>
+      </header>
+
+      {/* About Section */}
+      <div id="about" className="basic-1 bg-gray">
+        <div className="container">
+          <div className="row">
+            {/* Pendidikan */}
+            <div className="col-lg-4">
+              <div className="text-container first">
+                <h2>Pendidikan</h2>
+                {education.map((item, index) => (
+                  <div key={index}>
+                    <h6>{item.title}</h6>
+                    <h6>{item.subtitle}</h6>
+                    <div className="time">{item.time}</div>
+                  </div>
+                ))}
+
+                <br />
+                <h2>Sertifikasi</h2>
+                {certifications.map((item, index) => (
+                  <div key={index}>
+                    <div className="time">{item.time}</div>
+                    <h6>{item.title}</h6>
+                    {item.description && <p>{item.description}</p>}
+                  </div>
+                ))}
+              </div>
+            </div>
+
+            {/* Pengalaman */}
+            <div className="col-lg-4">
+              <div className="text-container second">
+                {experiences.map((item, index) => (
+                  <div key={index}>
+                    <div className="time">{item.time}</div>
+                    <h6>{item.title}</h6>
+                    <p>{item.description}</p>
+                  </div>
+                ))}
+              </div>
+            </div>
+
+            {/* Pencapaian */}
+            <div className="col-lg-4">
+              <div className="text-container third">
+                {achievements.map((item, index) => (
+                  <div key={index}>
+                    <div className="time">{item.time}</div>
+                    <h6>{item.title}</h6>
+                    <p>{item.description}</p>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
 export default About;
