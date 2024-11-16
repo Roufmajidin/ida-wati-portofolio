46d2aa51a62c877346be4805bdf1938d32166655
Rouf Majidin <roufmajidin777@gmail.com>
2024-11-16 23:25:30 +0700

    pew

--- src/components/project.js
+++ src/components/project.js
@@ -7,7 +7,7 @@ const Project = ()=>{
                 <div className="container">
                     <div className="row">
                         <div className="col-lg-12">
-                            <h2 className="h2-heading">Projek Saya Test</h2>
+                            <h2 className="h2-heading">ya jangan test atuh</h2>
                             <p className="p-heading">Beberapa Projek Selama Perkuliahan</p>
                         </div> {/* end of col */}
                     </div> {/* end of row */}
